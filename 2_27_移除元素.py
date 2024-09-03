c2ed58a00ec5ebacb21ea17e07650c5a95d03067
yu.xin <1405585468@qq.com>
2024-09-03 15:40:39 +0800

    2

--- 2_27_移除元素.py
+++ 2_27_移除元素.py
@@ -1,14 +1,13 @@
 def removeElement(nums, val):
-    # 快慢指针
-    fast = 0  # 快指针
-    slow = 0  # 慢指针
+    fast = 0  # 初始快指针
+    slow = 0  # 初始慢指针
     size = len(nums)
-    while fast < size:  # 不加等于是因为，a = size 时，nums[a] 会越界
-        # slow 用来收集不等于 val 的值，如果 fast 对应值不等于 val，则把它与 slow 替换
-        if nums[fast] != val: # 相当于对一个列表做两次记录，后一次的记录回更新当前的列表，哥哥排除烂瓜给弟弟好瓜
-            nums[slow] = nums[fast]
-            slow = slow + 1
-        fast = fast + 1
+
+    while fast < size:  # 当fast还没排除完时
+        if nums[fast] != val:  # 哥哥发现当前是好瓜
+            nums[slow] = nums[fast]  # 将瓜给弟弟
+            slow += 1  # 弟弟去下一个瓜坑等着
+        fast += 1  # 哥哥也去下一个瓜坑等着
     return slow
 
 
