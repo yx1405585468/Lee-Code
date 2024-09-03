c04cb35c98864e3115fc47b539ea7463b51cd9c8
yu.xin <1405585468@qq.com>
2024-09-03 16:00:41 +0800

    2

--- 3_977_有序数组的平方.py
+++ 3_977_有序数组的平方.py
@@ -1,16 +1,16 @@
-def removeElement(nums, val):
-    # 快慢指针
-    fast = 0  # 快指针
-    slow = 0  # 慢指针
-    size = len(nums)
-    while fast < size:  # 不加等于是因为，a = size 时，nums[a] 会越界
-        # slow 用来收集不等于 val 的值，如果 fast 对应值不等于 val，则把它与 slow 替换
-        if nums[fast] != val: # 相当于对一个列表做两次记录，后一次的记录回更新当前的列表，哥哥排除烂瓜给弟弟好瓜
-            nums[slow] = nums[fast]
-            slow = slow + 1
-        fast = fast + 1
-    return slow
+def sortedSquares(nums):
+    l, r, i = 0, len(nums)-1, len(nums)-1
+    res = [float('inf')] * len(nums) # 需要提前定义列表，存放结果
+    while l <= r:
+        if nums[l] ** 2 < nums[r] ** 2:  # 左右边界进行对比，找出最大值
+            res[i] = nums[r] ** 2
+            r -= 1  # 右指针往左移动
+        else:
+            res[i] = nums[l] ** 2
+            l += 1  # 左指针往右移动
+        i -= 1  # 存放结果的指针需要往前平移一位
+    return res
 
 
 if __name__ == '__main__':
-    print(removeElement(nums=[-1, 0, 3, 5, 9, 12], val=9))
+    print(sortedSquares(nums=[-4, -1, 0, 3, 10]))
