c88ef292e5cbb7704a319824b61cbee1b063c849
yu.xin <1405585468@qq.com>
2024-09-04 10:18:28 +0800

    2

--- 数组/7_开发商买房.py
+++ 数组/7_开发商买房.py
@@ -1,13 +1,21 @@
 def main():
+    # 输入数据
     data = input().split()
 
+    # 读取n行
     idx = 0
     n = int(data[idx])
+
+    # 读取m列
     idx += 1
     m = int(data[idx])
+
+    # 计算权重总值
     idx += 1
-    sum = 0
     vec = []
+    sum = 0
+
+    # 做二维数组
     for i in range(n):
         row = []
         for j in range(m):
@@ -18,31 +26,25 @@ def main():
         vec.append(row)
     print(vec)
     print(sum)
-
     result = float('inf')
-
     count = 0
     # 行切分
     for i in range(n):
-
         for j in range(m):
             count += vec[i][j]
-            # 遍历到行末尾时候开始统计
             if j == m - 1:
-                result = min(result, abs(sum - 2 * count))
+                result = min(result, abs(sum - count * 2))
 
-    count = 0
     # 列切分
+    count = 0
     for j in range(m):
-
         for i in range(n):
             count += vec[i][j]
-            # 遍历到列末尾时候开始统计
             if i == n - 1:
-                result = min(result, abs(sum - 2 * count))
+                result = min(result, abs(sum - count * 2))
 
     print(result)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
