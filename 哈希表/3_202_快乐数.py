e4489092aa452f440a86ab508a26018d7b423a47
yu.xin <1405585468@qq.com>
2024-09-23 12:43:45 +0800

    1

---
+++ 哈希表/3_202_快乐数.py
@@ -0,0 +1,17 @@
+def isHappy(n: int) -> bool:
+    record = set()
+    while n not in record:
+        record.add(n)
+        new_num = 0
+        n_str = str(n)  # 将正整数n转为字符串，这样子方便对每一位取值
+        for i in n_str:
+            new_num += int(i) ** 2
+        if new_num == 1:
+            return True
+        else:
+            n = new_num
+    return False
+
+
+if __name__ == '__main__':
+    print(isHappy(99))
