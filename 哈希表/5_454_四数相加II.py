6af9054b26a75014247e3254209836b9cda09d10
yu.xin <1405585468@qq.com>
2024-09-23 15:24:02 +0800

    1

--- 哈希表/5_454_四数相加II.py
+++ 哈希表/5_454_四数相加II.py
@@ -16,20 +16,22 @@ def fourSumCount(nums1, nums2, nums3, nums4):
             if key in hashmap1:
                 count += hashmap1[key]
     return count
-# hashmap2 = dict()
-# for i in nums3:
-#     for j in nums4:
-#         if i + j not in hashmap2:
-#             hashmap2[i + j] = 1
-#         else:
-#             hashmap2[i + j] = hashmap2[i + j] + 1
-# count = 0
-# for k1, value1 in hashmap1.items():
-#     for k2, value2 in hashmap2.items():
-#         if k1 + k2 == 0:
-#             count = count + (value1 * value2)
-#
-# print(count)
+
+    # 自己写的求法
+    # hashmap2 = dict()
+    # for i in nums3:
+    #     for j in nums4:
+    #         if i + j not in hashmap2:
+    #             hashmap2[i + j] = 1
+    #         else:
+    #             hashmap2[i + j] = hashmap2[i + j] + 1
+    # count = 0
+    # for k1, value1 in hashmap1.items():
+    #     for k2, value2 in hashmap2.items():
+    #         if k1 + k2 == 0:
+    #             count = count + (value1 * value2)
+    #
+    # print(count)
 
 
 if __name__ == '__main__':
