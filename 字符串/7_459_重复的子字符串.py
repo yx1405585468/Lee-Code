7faedb0b5abfb3404247401e763bfaa9ba3257b9
yu.xin <1405585468@qq.com>
2024-09-27 10:34:27 +0800

    1

--- 字符串/7_459_重复的子字符串.py
+++ 字符串/7_459_重复的子字符串.py
@@ -1,20 +1,24 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
-        return self.get_next(s)
+        if len(s) == 0:
+            return False
+        next = self.get_next(s)
+        if next[-1] != 0 and len(s) % (len(s) - next[-1]) == 0:
+            return True
+        return False
 
     def get_next(self, s):
-        result = [0] * len(s)
-
+        next = [0] * len(s)
+        i = 0
+        next[0] = 0
         for j in range(1, len(s)):
-            i = 0
-            location = j
-            k = 0
-            while s[i] == s[j] and i < j:
-                k += 1
+            while i > 0 and s[j] != s[i]:
+                i = next[i - 1]  # 往前退一步
+            if s[j] == s[i]:
                 i += 1
-                j -= 1
-            result[location] = k
-        return result
+            next[j] = i
+
+        return next
 
 
 if __name__ == '__main__':
